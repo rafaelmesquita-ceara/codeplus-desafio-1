64e34371a197da8bfed6a3f5a5aac663a30bbc0c
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-12-07 16:08:05 -0300

    Desafio concluído

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const cenario1 = [0, 0, 1, 0, 0, 1, 0];
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
 const jumpingOnClouds = c => {
+  var pulos = 0;
 
+  for (var i = 0; i < c.length; i++) {
+    //Caso a próximas duas nuvens sejam puláveis, contará apenas 1 pulo e pulará duas nuvens
+    if ((c[i + 1] === 0 && c[i + 2] === 0) || c[i + 1] === 1) { // OU caso a próxima nuvem seja não pulável também contará como um pulo e pulará duas nuvens
+      pulos++;
+      i++;
+    } else if (c[i + 2] === 1) { // Caso a segunda nuvem após a atual não seja pulável, contará um pulo e pulará apenas uma casa
+      pulos++;
+    }
+  }
+  //console.log(pulos);
+  return(pulos);
 }
 
 jumpingOnClouds(cenario1)
